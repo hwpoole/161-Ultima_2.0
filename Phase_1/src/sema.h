2d9062c5ead8e287e951fd9f90e7a1e9d84cad5e
Hunter Poole <punterhoole@gmail.com>
2026-03-20 21:13:45 -0400

    Add class semaphore template from asngmt document

---
+++ Phase_1/src/sema.h
@@ -0,0 +1,25 @@
+/* Semaphore Class
+ * Ultima 2.0
+ *
+ * 03-20-2026
+ */
+
+#pragma once
+
+#include <queue>
+
+using namespace std;
+
+class semaphore {
+private:
+  char resource_name[64];
+  int sema_value;
+  queue<char> *sema_queue;
+
+public:
+  bool down() {}
+
+  bool up() {}
+
+  void dump(int level) {}
+};
