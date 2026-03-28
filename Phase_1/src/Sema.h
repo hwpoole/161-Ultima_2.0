14d028001f0807b2a6ddfaa22ceeaf40f1dcc361
Hunter Poole <punterhoole@gmail.com>
2026-03-27 21:43:35 -0400

    Add Semaphore constructor from Lab10 example

--- Phase_1/src/Sema.h
+++ Phase_1/src/Sema.h
@@ -6,6 +6,7 @@
 
 #pragma once
 
+#include <pthread.h>
 #include <queue>
 
 using namespace std;
@@ -15,8 +16,15 @@ private:
   char resource_name[64];
   int sema_value;
   queue<int> *sema_queue;
+  pthread_mutex_t lock;
+  pthread_cond_t cond;
 
 public:
+  Semaphore(int initialValue) : sema_value(initialValue) {
+    pthread_mutex_init(&lock, nullptr);
+    pthread_cond_init(&cond, nullptr);
+  }
+
   bool down() {
     if (sema_value == 1) {
       sema_value = 0;
