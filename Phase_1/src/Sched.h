7b888eec7494ba096fe7eddf795774fd3fc8ca83
Hunter Poole <punterhoole@gmail.com>
2026-03-28 13:49:49 -0400

    Setup `Sched.h` docs block

---
+++ Phase_1/src/Sched.h
@@ -0,0 +1,15 @@
+/* Scheduler Class Header File
+ * Ultima 2.0
+ *
+ * This Scheduler class header file shows the private and public resources of
+ * the Scheduler class.
+ *
+ * Of note to the public, who wish to use this class:
+ *  1.
+ *
+ * Of note on the private resources:
+ *  1.
+ *
+ * Hunter Poole
+ * 03-28-2026
+ */
