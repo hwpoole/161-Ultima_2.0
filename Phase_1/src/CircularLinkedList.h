9a7ffcf3650083053fbdb48da2cd1f207f377b6c
Hunter Poole <punterhoole@gmail.com>
2026-03-28 15:16:07 -0400

    Setup CircularLinkedList comment block

---
+++ Phase_1/src/CircularLinkedList.h
@@ -0,0 +1,15 @@
+/* Circular Linked List Header File
+ * Ultima 2.0
+ *
+ * This header file shows the private and public resources of the
+ * CircularLinkedList class.
+ *
+ * Of note to the public, who wish to use this class:
+ *   1.
+ *
+ * Of note on the private resources:
+ *   1.
+ *
+ * Hunter Poole
+ * 03-28-2026
+ */
