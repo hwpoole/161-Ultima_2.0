fd322924a09a37b1b661cf52e533cafba7447898
Hunter Poole <punterhoole@gmail.com>
2026-03-29 23:28:30 -0400

    documentation

--- Phase_1/src/Ultima.cpp
+++ Phase_1/src/Ultima.cpp
@@ -1,31 +1,55 @@
 /* 161-Ultima 2.0 Phase 1 Demo file.
  *
+ * This file is intended only the demonstrate the results of phase 1.
+ * It is what I would consider to be "quite sloppy," and is maybe best deleted
+ * after this phase is completed or heavily reworked.
+ *
  * Hunter Poole
  * 03-29-2026
  */
 
 #include "Sched.h"
 #include "Sema.h"
+#include <chrono>
 #include <cstring>
 #include <ncurses.h>
 #include <stdarg.h>
+#include <thread>
 #include <unistd.h>
 
 using namespace std;
 
+//------------------Globals & forward declarations---------------
 Scheduler *my_scheduler = new Scheduler();
-Semaphore *my_semaphore = new Semaphore("The Thing");
+Semaphore *my_semaphore = new Semaphore("The Resource");
 
+// The Tasks used in this demo.
 int Task1 = my_scheduler->create_task();
 int Task2 = my_scheduler->create_task();
 int Task3 = my_scheduler->create_task();
 
+// The Task windows.
 WINDOW *Task1_Win;
 WINDOW *Task2_Win;
 WINDOW *Task3_Win;
+//-------------------End Globals & Forwards----------------------
 
+/* A faked program counter for the tasks.
+ * Was faster (and sillier) than changing the TCB and providing a getter/setter
+ *
+ * This is used entirely for the fake_work_with_semaphore() demo method.
+ */
 static int task_PCs[4] = {0, 0, 0, 0};
 
+/* WINDOW *create_window(int height, int width, int starty, int startx) {...}
+ *
+ * Helper method for creating windows from Lab 4.
+ *
+ * 1. Creates a WINDOW *Win pointer.
+ * 2. Sets its atributes according to the method's arguments.
+ * 3. Sets other desireable attributes.
+ * 4. Returns Win.
+ */
 WINDOW *create_window(int height, int width, int starty, int startx) {
   WINDOW *Win;
 
@@ -38,63 +62,121 @@ WINDOW *create_window(int height, int width, int starty, int startx) {
   return Win;
 }
 
+/* void write_window(WINDOW *Win, const char *text) {...}
+ *
+ * A helper method to write to the window from Lab 4.
+ *
+ * 1. Call wprintw on the window with the text.
+ * 2. Calls box 0,0 on the window.
+ * 3. Refreshes the window.
+ */
 void write_window(WINDOW *Win, const char *text) {
   wprintw(Win, text);
   box(Win, 0, 0);
   wrefresh(Win);
 }
 
+/* void write_window(WINDOW *Win, int y, int x, const char *text) {...}
+ *
+ * A helper method to write to the window from Lab 4.
+ * An overloaded version of the above method.
+ *
+ * 1. Call mvwprintw on the Window with x,y cords and text.
+ * 2. Calls box 0,0 on the window.
+ * 3. Refreshes the window.
+ */
 void write_window(WINDOW *Win, int y, int x, const char *text) {
   mvwprintw(Win, y, x, text);
   box(Win, 0, 0);
   wrefresh(Win);
 }
 
+/* void display_help(WINDOW *Win) {...}
+ *
+ * A helper method to display the help menu from Lab 4.
+ * In this version, we display two demo scenarios to show off the scheduler and
+ * semaphore.
+ *
+ * 1. Clears the window (likely Console_Win)
+ * 2. Writes a series of commands and their consequences to the Console_Win.
+ */
 void display_help(WINDOW *Win) {
   wclear(Win);
   write_window(Win, 1, 1, "...Help...");
   write_window(Win, 2, 1, "1= Scenario 1");
   write_window(Win, 3, 1, "2= Secnario 2");
-  write_window(Win, 4, 1, "3= Scenario 3");
-  write_window(Win, 5, 1, "c= clear screen");
-  write_window(Win, 6, 1, "h= help screen");
-  write_window(Win, 7, 1, "q= Quit");
+  // write_window(Win, 4, 1, "3= Scenario 3");
+  write_window(Win, 4, 1, "c= Clear screen");
+  write_window(Win, 5, 1, "h= Help screen");
+  write_window(Win, 6, 1, "q= Quit");
 }
 
+/* void *fake_work() {...}
+ *
+ * This method is a demo method to show off the strict round-robin scheduler in
+ * particular.
+ *
+ * 1. Get the current task's task_id from the scheduler.
+ * 2. Determine which Window that task relates to.
+ * 3. Determine what that task is called in a string.
+ * 4. "Run" that task 10 times.
+ * 5. Yield the task.
+ */
 void *fake_work() {
   int current_task = my_scheduler->get_task_id();
+  WINDOW *win = (current_task == Task1)   ? Task1_Win
+                : (current_task == Task2) ? Task2_Win
+                                          : Task3_Win;
+  const char *name = (current_task == Task1)   ? "Task 1"
+                     : (current_task == Task2) ? "Task 2"
+                                               : "Task 3";
   char buffer[256];
-  WINDOW *current_window = nullptr;
-  string TaskName;
-
-  if (current_task == Task1) {
-    current_window = Task1_Win;
-    TaskName = "Task 1";
-  } else if (current_task == Task2) {
-    current_window = Task2_Win;
-    TaskName = "Task 2";
-  } else if (current_task == Task3) {
-    current_window = Task3_Win;
-    TaskName = "Task 3";
-  }
-
-  char task[7];
-  strcpy(task, TaskName.c_str());
 
   for (int i = 0; i < 10; i++) {
-    sprintf(buffer, " %s running\n", task);
-    write_window(current_window, buffer);
-    sleep(1);
+    sprintf(buffer, " %s running\n", name);
+    write_window(win, buffer);
+    this_thread::sleep_for(chrono::milliseconds(500));
   }
 
-  sprintf(buffer, " %s yielding\n", task);
-  write_window(current_window, buffer);
-  sleep(1);
+  sprintf(buffer, " %s yielding\n", name);
+  write_window(win, buffer);
+  this_thread::sleep_for(chrono::milliseconds(500));
   my_scheduler->yield();
 
   return (NULL);
 }
 
+/* void *fake_work_with_semaphore() {...}
+ *
+ * This method is a demo method to show off how the semaphore blocks tasks and
+ * adds them to the queue. Of note, tasks do not run in *strict* FIFO on the
+ * scheduler, but do have a *strict* FIFO to the semaphore.
+ *
+ * We use the task_PCs from earlier to fake a program counter for running this
+ * method in a loop.
+ *
+ * 1. Get the current task's task id.
+ * 2. Determine what window that relates to.
+ * 3. Determine what that task is called.
+ * 4. Make a big switch case.
+ * 5. If program counter is 0:
+ *    - Call down() on semaphore.
+ *    - Increment PC.
+ *    - Return null if blocked to skip yield.
+ *    - Yield.
+ * 6. If 1:
+ *    - Declare access to critical region.
+ *    - Increment PC.
+ *    - Yield.
+ * 7. If 2:
+ *    - Release semaphore.
+ *    - Increment PC.
+ *    - Yield.
+ * 8. If 3:
+ *    - Declare finished.
+ *    - Increment PC.
+ * 9. Yield.
+ */
 void *fake_work_with_semaphore() {
   int current_task = my_scheduler->get_task_id();
   WINDOW *win = (current_task == Task1)   ? Task1_Win
@@ -115,10 +197,12 @@ void *fake_work_with_semaphore() {
     pc++;
 
     if (my_scheduler->get_state(current_task) == BLOCKED) {
+      sprintf(buffer, " %s was blocked\n", name);
+      write_window(win, buffer);
       return (NULL);
     }
 
-    sleep(1);
+    this_thread::sleep_for(chrono::milliseconds(500));
     my_scheduler->yield();
     break;
   case 1:
@@ -126,7 +210,7 @@ void *fake_work_with_semaphore() {
     write_window(win, buffer);
     pc++;
 
-    sleep(1);
+    this_thread::sleep_for(chrono::milliseconds(500));
     my_scheduler->yield();
     break;
   case 2:
@@ -135,7 +219,7 @@ void *fake_work_with_semaphore() {
     my_semaphore->up();
     pc++;
 
-    sleep(1);
+    this_thread::sleep_for(chrono::milliseconds(500));
     my_scheduler->yield();
     break;
   case 3:
@@ -143,7 +227,7 @@ void *fake_work_with_semaphore() {
     write_window(win, buffer);
     pc++;
 
-    sleep(1);
+    this_thread::sleep_for(chrono::milliseconds(500));
     break;
   }
 
@@ -151,16 +235,35 @@ void *fake_work_with_semaphore() {
   return (NULL);
 }
 
+/* int main() {...}
+ *
+ * main method to string all of this together.
+ *
+ * 1. Registers the scheduler with the semaphore class.
+ * 2. Sets quantum *very* low for the sake of the demo to force yields to
+ *    happen on command.
+ * 3. Initializes the screen.
+ * 4. Creates and prints the Heading window.
+ * 5. Creates and prints the log window.
+ * 6. Creates and prints the console window.
+ * 7. Creates and prints the task windows.
+ * 8. Starts the scheduler.
+ * 9. Sets up I/O processing for ncurses.
+ * 10.Runs I/O processing in a while loop.
+ * 11.Big switch case for I/O options.
+ */
 int main() {
+  // Register scheduler with semaphore and set quantum LOW To force yields.
   Semaphore::set_scheduler(my_scheduler);
   my_scheduler->set_quantum(1);
 
+  // Initializes the ncurses screen.
   initscr();
 
+  // Create and print Heading_Win.
   WINDOW *Heading_Win = newwin(12, 80, 3, 2);
   box(Heading_Win, 0, 0);
   mvwprintw(Heading_Win, 2, 28, "161-ULTIMA 2.0 PHASE 1 DEMO");
-
   mvwprintw(Heading_Win, 4, 2, "Starting demo...");
   mvwprintw(Heading_Win, 5, 2, "Starting Task 1...");
   mvwprintw(Heading_Win, 6, 2, "Starting Task 2...");
@@ -168,14 +271,17 @@ int main() {
   mvwprintw(Heading_Win, 9, 2, "Press 'q' or Ctrl-C to exit the program...");
   wrefresh(Heading_Win);
 
+  // Create and print Log_Win.
   WINDOW *Log_Win = create_window(10, 60, 30, 2);
   write_window(Log_Win, 1, 5, "...Log Window...\n");
   write_window(Log_Win, "...Main program started...\n");
 
+  // Create and print Console_Win.
   WINDOW *Console_Win = create_window(10, 20, 30, 62);
   write_window(Console_Win, 1, 1, "...Console...");
   write_window(Console_Win, 2, 1, "161-Ultima 2.0 #");
 
+  // Create and print all Task<x>_Win.
   Task1_Win = create_window(15, 25, 15, 2);
   write_window(Task1_Win, 6, 1, "Task 1 Window\n");
   Task2_Win = create_window(15, 25, 15, 30);
@@ -183,6 +289,7 @@ int main() {
   Task3_Win = create_window(15, 25, 15, 57);
   write_window(Task3_Win, 6, 1, "Task 3 Window\n");
 
+  // Call to start the scheduler.
   my_scheduler->start();
 
   // I/O processing
@@ -194,18 +301,19 @@ int main() {
   char buffer[256];
   int input = -1;
 
+  // Collect input until user quits.
   while (input != 'q') {
     input = wgetch(Console_Win);
 
     switch (input) {
-    case '1':
+    case '1': // Scenario 1.
       write_window(Log_Win, " Scenario 1: One Task At A Time\n");
       for (int i = 0; i < 3; i++) {
         fake_work();
       }
       write_window(Log_Win, " Scenario 1 Finished\n");
       break;
-    case '2':
+    case '2': // Scenario 2.
       write_window(Log_Win,
                    " Scenario 2: Each Task wants the critical region\n");
       for (int i = 0; i < 30; i++) {
@@ -217,9 +325,9 @@ int main() {
         task_PCs[i] = 0;
       }
       break;
-    case '3':
+    case '3': // Incomplete Scenario 3.
       break;
-    case 'c':
+    case 'c': // Clear windows.
       wclear(Console_Win);
       wclear(Task1_Win);
       wclear(Task2_Win);
@@ -229,14 +337,15 @@ int main() {
       write_window(Log_Win, "...Main program started...\n");
       write_window(Console_Win, 1, 1, "161-Ultima 2.0 # ");
       break;
-    case 'h':
+    case 'h': // Display help.
       display_help(Console_Win);
       write_window(Console_Win, 8, 1, "161-Ultima 2.0 # ");
       break;
-    case 'q':
+    case 'q': // q for quit!
       write_window(Log_Win, " Quitting...\n");
       write_window(Log_Win, " Call kill_task on all tasks.\n");
 
+      /*
       for (int i = 0; i < 3; i++) {
         int this_task = my_scheduler->get_task_id();
         my_scheduler->kill_task();
@@ -248,11 +357,12 @@ int main() {
           write_window(Task3_Win, " Killed Task 3\n");
         }
       }
+      */
 
       break;
-    case ERR:
+    case ERR: // Gracefully handle errors.
       break;
-    default:
+    default: // Default behavior on unregistered inputs.
       sprintf(buffer, " %c", input);
       write_window(Console_Win, buffer);
       write_window(Console_Win, " -Invalid Command\n");
