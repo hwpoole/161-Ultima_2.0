50423d77d3762ebaa27ad0168ea7efe2565f2eb8
Khasria16 <manpreetk1661@gmail.com>
2026-04-06 21:10:19 -0400

    Added initial Pipe.h structure

---
+++ src/Pipe.h
@@ -0,0 +1,7 @@
+#ifndef PIPE_H               // Prevents multiple inclusion of this header file
+#define PIPE_H
+
+#include <iostream>          // For cout (used in dump function)
+using namespace std;
+
+#define PIPE_SIZE 32         // Maximum size of the pipe buffer (as per assignment)
