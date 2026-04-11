37fb72fd61eea269fa88b05921e7e056fd1c8c23
Hunter Poole <punterhoole@gmail.com>
2026-04-11 19:55:21 -0400

    Update Uthread docs

--- src/Uthread.h
+++ src/Uthread.h
@@ -5,6 +5,16 @@
  * It was created to repalce calls to the pthread library, such that working
  * with the Ultima 2.0 OS would be easy, and based off prior knowledge.
  *
+ * Public methods:
+ * 1. uthread()
+ *    - A no-arg constructor.
+ * 2. int create()
+ *    - A call to create a uthread and dispatch it, with args.
+ * 3. int join()
+ *    - A call to join a uthead.
+ * 4. uthread_t self()
+ *    - Returns the uthread_t for the calling uthread.
+ *
  * Hunter Poole
  * 04-05-2026
  */
@@ -16,6 +26,7 @@
 
 using namespace std;
 
+// uthread_t is an alias for pthread_t.
 typedef pthread_t uthread_t;
 
 class uthread {
