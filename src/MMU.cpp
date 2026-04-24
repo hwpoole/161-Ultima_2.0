106800ff10a4d0abfcc35e367b41c7eefd328770
Hunter Poole <punterhoole@gmail.com>
2026-04-24 00:06:56 -0400

    Add `Mem_Alloc()`, and `Block()` constructor.

--- src/MMU.cpp
+++ src/MMU.cpp
@@ -7,6 +7,20 @@
 
 #include "MMU.h"
 
+/* Block() {...}
+ *
+ * No-arg constructor.
+ *
+ * Primary benefit of using this is to have Bytes be initialized to '.'
+ *
+ * 1. Sets owner to NULL.
+ * 2. Fills all bytes with '.'
+ */
+MMU::Block::Block() {
+  owner = NULL;
+  fill(begin(Bytes), end(Bytes), '.');
+}
+
 /* MMU() {...}
  *
  * No-arg constructor.
@@ -15,18 +29,15 @@
  * Since we are allocating these blocks "on boot," we don't yet have an owner
  * for them.
  *
- * Nested for-loops. Do this 16x.
+ * Do this 16x:
  *  1. Create a new Block pointer.
- *     2. Initialize all bytes to '.'
- *  3. Insert that block into Memory.
+ *  2. Insert that block into Memory.
  */
 MMU::MMU() {
   for (int i = 0; i < 15; i++) {
     Block *NewBlock = new Block();
-    for (int j = 0; j < 63; j++) {
-      NewBlock->Bytes[j] = '.';
-    }
     Memory.insert_front(NewBlock);
+    Free.push_back(true);
   }
 }
 
@@ -48,4 +59,51 @@ MMU::~MMU() {
     delete DeadBlock;
     Memory.remove_front();
   }
+}
+
+/* Mem_Alloc(int size) {...}
+ *
+ * Assigns memory blocks to the requesting task based on the size argument.
+ * Finds empty blocks in Memory and assigns them to the task until there are no
+ * more empty blocks OR we have allocated all that the requesting task has asked
+ * for.
+ *
+ * 1. Get an Empty block for comparison.
+ * 2. Get the start position.
+ * 3. Find an empty block, if one exists.
+ * 4. Make an int 'allocated' to track allocated bytes.
+ * 5. While we have more bytes to allocate, and empty blocks exist...
+ *    5a. Move to an empty block.
+ *    5b. Set that block's owner as this thread via pthread_self().
+ *    5c. Increment allocated by 64 (block size).
+ *    5d. Decrement size by 64.
+ *    5e. Determine if there is another empty block.
+ * 6. If the start position is not the current head and still exists...
+ *    6a. Move back to the start position.
+ * 7. Return the number of allocated bytes.
+ */
+int MMU::Mem_Alloc(int size) {
+  Block *FindEmpty;
+  Block *StartBlock = Memory.get_front();
+  int pos = Memory.get_pos(FindEmpty);
+  int allocated = 0;
+
+  while (size > 0 && pos != -1) {
+    Memory.move_to_key(FindEmpty);
+
+    FindEmpty->owner = pthread_self();
+    Memory.set_value(FindEmpty);
+    allocated += 64;
+    size -= 64;
+    FindEmpty->owner = NULL;
+
+    pos = Memory.get_pos(FindEmpty);
+  }
+
+  // Possible that start pos was empty and allocated to the requesting task.
+  if (StartBlock != Memory.get_front() && Memory.get_pos(StartBlock) != -1) {
+    Memory.move_to_key(StartBlock);
+  }
+
+  return allocated;
 }
--- src/MMU.h
+++ src/MMU.h
@@ -8,18 +8,26 @@
 #pragma once
 
 #include "CircularLinkedList.h"
+#include <vector>
+
+using namespace std;
 
 class MMU {
 private:
   struct Block {
     pthread_t owner;
     char Bytes[64];
+
+    Block();
   };
 
   CircularLinkedList<Block *> Memory;
+  vector<bool> Free;
 
 public:
   MMU();
 
   ~MMU();
+
+  int Mem_Alloc(int size);
 };
