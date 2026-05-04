7f5d9741ffcaa2bbde20cef80041a16d36d41911
Hunter Poole <punterhoole@gmail.com>
2026-05-04 00:00:59 -0400

    Adding what I have.

---
+++ Phases/Phase_4/src/Sema.h
@@ -0,0 +1,81 @@
+/* Semaphore Class Header File
+ * Ultima 2.0
+ *
+ * This Semaphore header file shows the private and public resources of the
+ * Semaphore class.
+ *
+ * Of note to the public, who wish to use this class:
+ *
+ *  1. Semaphore(const char *Name);
+ *      - A constructor. Takes the name of the resource the Semaphore manages.
+ *  2. ~Semaphore();
+ *      - A destructor.
+ *  3. void set_scheduler(Scheduler *s);
+ *      - Registers the Scheduler with the Semaphore class.
+ *  4. void down();
+ *      - Call this to attempt to lock and access the guarded resource.
+ *      - Threads that can't get access to the resource will wait in a FIFO
+ *        queue until they are called on.
+ *      - This is a blocking wait, but is not a busy wait.
+ *  5. void down_if();
+ *      - Allows a thread to volunteer itself to the semaphore queue if a
+ *        condition is met.
+ *      - Threads that can't get access to the resource will wait in the same
+ *        FIFO queue as down();
+ *      - Blocking, but not busy wait.
+ *  6. void up();
+ *      - Call this to unlock the guarded resource for use by other threads.
+ *  7. string dump();
+ *      - Call this to see the current contents and/or state of the Semaphore.
+ *
+ *
+ * Of note on the private resources:
+ *
+ * 1. string resource_name;
+ *    - Stores the name of the resource the Semaphore manages.
+ *    - This is set *once* via the constructor.
+ * 2. int sema_value = 1;
+ *    - Any valid binary Semaphore should start its life as "available."
+ *      As in, you may not simultaneously create and lock a Semaphore.
+ *      Thus, it is "up" and available for use from the get-go.
+ * 3. queue<int> sema_queue;
+ *    - Stores the task_ids for the tasks waiting for access on the
+ *      guarded resource.
+ *    - Is a FIFO queue. Tasks may not proceed unless they are in the front.
+ * 4. static Scheduler *scheduler;
+ *    - Stores a pointer to the scheduler.
+ *    - Required to communicate with the scheduler.
+ * Hunter Poole
+ * 03-28-2026
+ */
+
+#pragma once
+
+#include "Sched.h"
+#include <queue>
+#include <string>
+
+using namespace std;
+
+class Semaphore {
+private:
+  string resource_name;
+  int sema_value = 1;
+  queue<int> sema_queue;
+  static Scheduler *scheduler;
+
+public:
+  Semaphore(const char *Name);
+
+  ~Semaphore() = default;
+
+  static void set_scheduler(Scheduler *s) { scheduler = s; }
+
+  void down();
+
+  void down_if(bool condition);
+
+  void up();
+
+  string dump();
+};
